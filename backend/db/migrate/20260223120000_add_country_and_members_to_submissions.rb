fda8432572e596fed178c1bc5278e452796f9133
Yazdan Basir <yazdan.b.14@gmail.com>
2026-02-24 15:37:18 -0500

    Make add_country_and_members migration idempotent

--- backend/db/migrate/20260223120000_add_country_and_members_to_submissions.rb
+++ backend/db/migrate/20260223120000_add_country_and_members_to_submissions.rb
@@ -1,6 +1,6 @@
 class AddCountryAndMembersToSubmissions < ActiveRecord::Migration[8.1]
   def change
-    add_column :submissions, :country_code, :string
-    add_column :submissions, :members, :text
+    add_column :submissions, :country_code, :string unless column_exists?(:submissions, :country_code)
+    add_column :submissions, :members, :text unless column_exists?(:submissions, :members)
   end
 end
